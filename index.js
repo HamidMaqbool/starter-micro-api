0773768405555ba2b202dd437ba89c42774f1179
Hamid <hamid@live.it>
2023-09-24 10:18:37 +0500

    error

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ var axios = require('axios');
 
 http.createServer(async function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    var out = await axios.get('https://api3.binance.com/api/v3/myTrades');
+    var out = await axios.get('https://api3.binance.com/api/v3/myTrades') .then((response) => response.data)
+    .catch((error) => {
+    return 'error';
+    });
+
     res.write(out);
     res.end();
 }).listen(process.env.PORT || 3000);
