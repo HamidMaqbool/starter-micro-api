4e6f0ba5fcb3a5dbde4c350098cf439bc5522c1a
HamidMaqbool <hamidmuslim@live.com>
2023-09-24 09:51:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var http = require('http');
+var os = require('os');
+
+var networkInterfaces = os.networkInterfaces();
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('test');
+    res.write(networkInterfaces);
     res.end();
 }).listen(process.env.PORT || 3000);
