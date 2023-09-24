b188ddc14772c25b3ccc4d24f796c5a1487687b3
HamidMaqbool <hamidmuslim@live.com>
2023-09-24 09:53:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var http = require('http');
-var os = require('os');
 
-var networkInterfaces = os.networkInterfaces();
+function getIPAddress() {
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+        return alias.address;
+    }
+  }
+  return '0.0.0.0';
+}
+
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write(networkInterfaces);
+    res.write(getIPAddress());
     res.end();
 }).listen(process.env.PORT || 3000);
