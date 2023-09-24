c5f7671288f33aa2db033052e9f8877aa80a4fd6
Hamid <hamid@live.it>
2023-09-24 10:03:11 +0500

    test

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 var http = require('http');
 
-function getIPAddress() {
-  var interfaces = require('os').networkInterfaces();
-  for (var devName in interfaces) {
-    var iface = interfaces[devName];
-
-    for (var i = 0; i < iface.length; i++) {
-      var alias = iface[i];
-      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
-        return alias.address;
-    }
-  }
-  return '0.0.0.0';
-}
-
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write(getIPAddress());
+    res.write('testing');
     res.end();
 }).listen(process.env.PORT || 3000);
